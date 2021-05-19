e987217e9ac9544db80bf518c3ede642828d8c36
jinhee196 <84313420+jinhee196@users.noreply.github.com>
2021-05-19 15:46:01 +0900

    js: scrollIntoView

--- main.js
+++ main.js
@@ -11,3 +11,22 @@ document.addEventListener('scroll', () => {
         navbar.classList.remove('navbar--dark');
     }
 });
+
+const navbarMenu = document.querySelector('.navbar__menu');
+const contact_btn = document.querySelector('.home__contact');
+const contact = document.querySelector('#contact');
+
+navbarMenu.addEventListener('click', (event) => {
+    const target = event.target;
+    const link = target.dataset.link;
+       if(link == null) {
+           return;
+       }
+       const scrollTo = document.querySelector(link);
+       scrollTo.scrollIntoView({behavior: 'smooth'});
+
+});
+contact_btn.addEventListener('click', () => {
+    // window.scrollTo({top: 200, behavior: 'smooth'});
+    contact.scrollIntoView({behavior: 'smooth'});
+});
